08775fca4b830f2838619d1ce53f64a2f8dcb151
rajshiv-24 <rajputshivanshu589@gmail.com>
2025-08-07 10:13:05 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,16 +20,16 @@ const App = () => {
       <ToastContainer />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:speciality' element={<Doctors />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/appointment/:docId' element={<Appointment />} />
-        <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/verify' element={<Verify />} />
+        <Route path='/' element = {<Home />} />
+        <Route path='/doctors' element = {<Doctors />} />
+        <Route path='/doctors/:speciality' element = {<Doctors />} />
+        <Route path='/login' element = {<Login />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/contact' element = {<Contact />} />
+        <Route path='/appointment/:docId' element = {<Appointment />} />
+        <Route path='/my-appointments' element = {<MyAppointments />} />
+        <Route path='/my-profile' element = {<MyProfile />} />
+        <Route path='/verify' element = {<Verify />} />
       </Routes>
       <Footer />
     </div>
